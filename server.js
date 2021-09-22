b2f6394f4e1606e78c7a04a61255af24c987cba8
Alexis Guzman <alexisguzman157@outlook.com>
2021-09-22 01:12:09 -0400

    added uuid and correct .get and post

--- server.js
+++ server.js
@@ -1,38 +1,52 @@
 const express = require('express');
-//const index = require('./routes/html');
-const apiR = require('./routes/api');
-const htmlR = require ('./routes/html')
+const db = require('./db/db.json');
+const path = require('path');
+const fs = require('fs');
+const uuid =require('./uuid/uuid')
 
-const PORT = process.env.port || 3001;
 
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.use(express.static('public'));
 
 
-app.use(apiR);
-app.use(htmlR);
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"))
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"))
+});
 
 
 
+app.get("/api/notes", (req, res) => {
+  res.json(db)
+});
+
+app.post("/api/notes", (req, res) => {
+
+  fs.writeFileSync('db/db.json', JSON.stringify(db))
+
+  res.json("new note")
+})
 
 
-// app.get('/', (req, res) =>
-//   res.sendFile(path.join(__dirname, '/public/index.html'))
-// );
+// app.delete("/api/notes/:id", (req, res)=> {
 
-// app.get('/feedback', (req, res) =>
-//   res.sendFile(path.join(__dirname, '/public/notes.html/feedback.html'))
-// );
-
-// app.get('*', (req, res) =>
-//   res.sendFile(path.join(__dirname, 'public/pages/404.html'))
-// ); NOT sure if needed
+// }
+// )
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
+
+// pulled from activity 17
+// module.exports = () =>
+//   Math.floor((1 + Math.random()) * 0x10000)
+//     .toString(16)
+//     .substring(1);
--- uuid/uuid.js
+++ uuid/uuid.js
@@ -2,3 +2,4 @@ module.exports = () =>
   Math.floor((1 + Math.random()) * 0x10000)
     .toString(16)
     .substring(1);
+//used from activity 20
